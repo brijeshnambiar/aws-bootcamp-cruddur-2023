d7a752e2674d17e1e99c135f4a3876ebfc016c12
brijeshnambiar <brijeshnambiar@gmail.com>
2023-03-18 08:06:40 +0000

    refactor db lib

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -1,25 +1,76 @@
 from psycopg_pool import ConnectionPool
 import os
 
-connection_url = os.getenv("CONNECTION_URL")
-pool = ConnectionPool(connection_url)
+class DB:
+  def __init__(self):
+    self.init_pool()
 
-def query_wrap_object(template):
-  sql = f"""
-  (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
-  {template}
-  ) object_row);
-  """
-  return sql
+  def init_pool(self):
+    connection_pool = os.getenv("CONNECTION_URL")
+    self.pool = ConnectionPool(connection_pool)
 
-def query_wrap_array(template):
-  sql = f"""
-  (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
-  {template}
-  ) array_row);
-  """
-  return sql
+  def query_commit(self):
+    try:
+      conn = self.pool.connection()
+      cur = conn.cursor()
+      cur.execute(sql)
+      conn.commit()
+    except Exception as err:
+      print_sql_err(err)
+      #conn.rollback()
+
+  def query_array_json(self,sql):
+    print("SQL Statement - [array]...")
+    print(sql + "\n")
+    wrapped_sql = self.query_wrap_array(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+
+  def query_object_json(self, sql):
+    print("SQL Statement - [object]...")
+    print(sql + "\n")
+    wrapped_sql = self.query_wrap_object(sql)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(wrapped_sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        return json[0]
+  
+
+  def query_wrap_object(self,template):
+    sql = f"""
+    (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
+    {template}
+    ) object_row);
+    """
+    return sql
+
+  def query_wrap_array(self,template):
+    sql = f"""
+    (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
+    {template}
+    ) array_row);
+    """
+    return sql
+
+  def print_sql_err(self,err):
+    err_type, err_obj, traceback = sys.exc_info()
+    line_num = traceback.tb_lineno
+
+    print("\n psycopg ERROR", err, "on line number:", line_num)
+    print("psycopg traceback:", traceback, "-- type:", err_type)
+
+    print("\nextensions.Diagnostics:", err.diag)
+
+    print("pgerrors:", err.pgerror)
+    print("pgcode:", err.pgcode, "\n")
 
 
-
-
+db = DB()
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool,query_wrap_object, query_wrap_array
+from lib.db import db
 
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
@@ -13,7 +13,7 @@ class HomeActivities:
       #span.set_attribute("app.now", now.isoformat())
         # do something
       
-      sql = query_wrap_array("""
+      results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -29,10 +29,4 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
+      return results
